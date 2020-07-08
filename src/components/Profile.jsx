dc15a386f8de8845bfe452b4e8b1a860cfc38582
Max Ivanov <maximusdrum@gmail.com>
2020-07-08 19:02:41 +0300

    Разбил стили на модули

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <main className='content'>
+        <main className={s.content}>
             <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' />
 
             <div> avatar + description</div>
@@ -11,11 +12,11 @@ const Profile = () => {
             <div>
                 New Post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                 post1
                 </div>
-                <div>
+                <div className={s.item}>
                 post2
                 </div>
             </div>
