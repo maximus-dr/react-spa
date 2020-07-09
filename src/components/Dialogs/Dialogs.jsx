0fb6dfcece53368d0681e364cdbad30be80c59a3
Max Ivanov <maximusdrum@gmail.com>
2020-07-09 11:06:24 +0300

    Разбил блок Dialogs на несколько колонок

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-         <div>
-             Dialogs
+         <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                 <div className={s.dialog + ' ' + s.active}>
+                     Kate
+                 </div>
+                 <div className={s.dialog}>
+                     Oleg
+                 </div>
+                 <div className={s.dialog}>
+                     Igor
+                 </div>
+                 <div className={s.dialog}>
+                     Viktor
+                 </div>
+                 <div className={s.dialog}>
+                     Valery
+                 </div>
+                 <div className={s.dialog}>
+                     Michael
+                 </div>
+                 <div className={s.dialog}>
+                     Denis
+                 </div>
+             </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Hello</div>
+            </div>
          </div>
     );
 }
