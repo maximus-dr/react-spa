8d3c3eaa0ba264fd0a17ea241e32f1e882c5ba6b
Max Ivanov <maximus-dr@mail.ru>
2020-07-12 00:37:26 +0300

    В state.js вынес все переменные и функции в объект store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addPost, updateNewPostText} from './redux/state';
 
 
 let rerenderEntireTree = (state) => {
@@ -12,16 +11,16 @@ let rerenderEntireTree = (state) => {
       <React.StrictMode>
         <App 
           state={state} 
-          addPost={addPost}
-          updateNewPostText={updateNewPostText} />
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)} />
       </React.StrictMode>,
       document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,57 +1,62 @@
-let rerenderEntireTree;
+let store = {
 
-let state = {
+    _state: {
 
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likes: 12},
-            {id: 2, message: 'It is my first post', likes: 4},
-            {id: 2, message: 'So beatiful', likes: 13}
-        ],
-        newPostText: 'comment'
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likes: 12},
+                {id: 2, message: 'It is my first post', likes: 4},
+                {id: 2, message: 'So beatiful', likes: 13}
+            ],
+            newPostText: 'comment'
+        },
+    
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Kate'},
+                {id: 2, name: 'Oleg'},
+                {id: 3, name: 'Igor'},
+                {id: 4, name: 'Valery'},
+                {id: 5, name: 'Angelina'},
+                {id: 6, name: 'Monica'},
+                {id: 7, name: 'Salma'} 
+            ],
+        
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you?'},
+                {id: 3, message: 'Bye'}
+            ]
+        }
     },
 
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Kate'},
-            {id: 2, name: 'Oleg'},
-            {id: 3, name: 'Igor'},
-            {id: 4, name: 'Valery'},
-            {id: 5, name: 'Angelina'},
-            {id: 6, name: 'Monica'},
-            {id: 7, name: 'Salma'} 
-        ],
-    
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Bye'}
-        ]
+    getState() {
+        return this._state;
+    },
+
+    _callSubscriber() {},
+
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likes: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    // pattern observer
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-};
-
-export const addPost = () => {
-
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likes: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-// pattern observer
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
