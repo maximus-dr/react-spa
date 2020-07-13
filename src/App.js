62a87119029ad06054d3f26dbfd897546e08792c
Max Ivanov <maximus-dr@mail.ru>
2020-07-13 18:10:08 +0300

    Добавил MyPostsContainer

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ const App = (props) => {
           <Route 
             path='/profile' 
             render={ () => 
-              <Profile 
-                profilePage={props.state.profilePage}
-                dispatch={props.dispatch} /> } 
+              <Profile store={props.store} /> } 
           />
 
           <Route 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Post from './Post/Post';
 import s from './MyPosts.module.css';
-import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
 
 
 const MyPosts = (props) => {
@@ -11,14 +10,13 @@ const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return (
@@ -32,7 +30,7 @@ const MyPosts = (props) => {
                         value={props.newPostText} />
                 </div>
                 <div>
-                    <button onClick={ addPost }>Add post</button>
+                    <button onClick={ onAddPost }>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
