03eef31c3377ec49bfb4cea86881486704f41304
Max Ivanov <maximus-dr@mail.ru>
2020-07-14 20:14:30 +0300

    В Users обернул запрос на сервер в функцию getUsers

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,16 +5,21 @@ import userPhoto from '../../assets/img/user.svg';
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
+
+
     
 
     return <div>
+        <button onClick={getUsers}>Get users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
