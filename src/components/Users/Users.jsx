7787a8673af7fd197c049f4a64db2196c198c434
Max Ivanov <maximus-dr@mail.ru>
2020-07-14 18:40:50 +0300

    Подключил axios, добавил запрос на сервер в Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,16 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/img/user.svg';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1, 
-                    photoUrl: 'https://image.flaticon.com/icons/svg/147/147144.svg', followed: false, 
-                    fullName: 'Maximus', 
-                    status: 'My status', 
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2, 
-                    photoUrl: 'https://image.flaticon.com/icons/svg/147/147144.svg', followed: true, 
-                    fullName: 'Olga', 
-                    status: 'My status', 
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3, photoUrl: 'https://image.flaticon.com/icons/svg/147/147144.svg', 
-                    followed: false, 
-                    fullName: 'Oleg', 
-                    status: 'My status', 
-                    location: {city: 'New-York', country: 'USA'}
-                },
-                {
-                    id: 4, 
-                    photoUrl: 'https://image.flaticon.com/icons/svg/147/147144.svg', followed: false, 
-                    fullName: 'Valery', 
-                    status: 'My status', 
-                    location: {city: 'Warsaw', country: 'Poland'}
-                },
-            ]
-        );
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
     
 
@@ -44,7 +19,7 @@ const Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={s.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} />
                     </div>
                     <div>
                         { u.followed 
@@ -55,12 +30,12 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
