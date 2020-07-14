75a63d67cc6855af34020ad51aeca7f7f459962d
Max Ivanov <maximus-dr@mail.ru>
2020-07-14 11:45:55 +0300

    Рефакторинг profileReducer и messagesReducer

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,10 +9,10 @@ const Dialogs = (props) => {
     let state = props.messagesPage;
 
     let dialogsElements = state.dialogs
-        .map( d => <DialogItem name={d.name} id={d.id} /> );
+        .map( d => <DialogItem name={d.name} key={d.id} id={d.id} /> );
 
     let messagesElements = state.messages
-        .map( m => <Message message={m.message} /> );
+        .map( m => <Message message={m.message} key={m.id} /> );
 
     let newMessageBody = state.newMessageBody;
 
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -14,22 +14,23 @@ const profileReducer = (state = initialState, action) => {
 
     switch(action.type) {
 
-        case ADD_POST:{
+        case ADD_POST:
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likes: 0
             };
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;}
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            }
 
-        case UPDATE_NEW_POST_TEXT:{
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;}
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            }
 
         default:
             return state;
