2e8ec571fe97301bae064f400486374f6739410b
Max Ivanov <maximus-dr@mail.ru>
2020-07-18 01:30:50 +0300

    Добавил login в Header

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,11 +3,13 @@ import messagesReducer from "./messages-reducer";
 
 import { createStore, combineReducers } from "redux";
 import UsersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
-    usersPage: UsersReducer
+    usersPage: UsersReducer,
+    auth: authReducer
 }); 
 
 let store = createStore(reducers);
