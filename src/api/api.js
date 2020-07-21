de008671ed9226701ab5483a6e581ca1447ba131
Max Ivanov <maximus-dr@mail.ru>
2020-07-21 17:48:27 +0300

    Добавил thunk в Header и Profile

--- src/api/api.js
+++ src/api/api.js
@@ -10,16 +10,27 @@ const instance = Axios.create({
 });
 
 export const usersAPI = {
+
     getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`,{
-                withCredentials: true
-        })
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data);
     },
+
     follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`);
+        return instance.post(`follow/${userId}`);
     },
+
     unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`);
+        return instance.delete(`follow/${userId}`);
+    },
+
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`);
+    }
+};
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`);
     }
 };
