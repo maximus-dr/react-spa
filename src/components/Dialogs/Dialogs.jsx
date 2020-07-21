1e81623fd34d366c25bd5055eabde1b7367bc97e
Max Ivanov <maximus-dr@mail.ru>
2020-07-21 19:08:34 +0300

    Добавил Redirect на /login в Profile и Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import { Redirect } from 'react-router-dom';
 
 
 const Dialogs = (props) => {
@@ -24,6 +25,8 @@ const Dialogs = (props) => {
         let body = evt.target.value;
         props.updateNewMessageBody(body);
     };
+
+    if (!props.isAuth) return <Redirect to={'/login'} />
 
     return (
          <div className={s.dialogs}>
