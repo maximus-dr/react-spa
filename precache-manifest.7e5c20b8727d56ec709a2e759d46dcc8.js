49cda872bf0fa4096589225f1267ab604e59534c
Max Ivanov <maximus-dr@mail.ru>
2020-10-08 12:41:14 +0300

    Updates

---
+++ precache-manifest.7e5c20b8727d56ec709a2e759d46dcc8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071b2588d45d011bd28e721a2b86ebf2",
+    "url": "/react-spa/index.html"
+  },
+  {
+    "revision": "76004d9429b772db1eb9",
+    "url": "/react-spa/static/css/3.c217ef28.chunk.css"
+  },
+  {
+    "revision": "761ed39bfaa91322b3a4",
+    "url": "/react-spa/static/css/4.d1abf58b.chunk.css"
+  },
+  {
+    "revision": "5453380a66379f18f028",
+    "url": "/react-spa/static/css/main.8fdebd9b.chunk.css"
+  },
+  {
+    "revision": "294c30331f8da5adbfe9",
+    "url": "/react-spa/static/js/2.5a139e46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spa/static/js/2.5a139e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76004d9429b772db1eb9",
+    "url": "/react-spa/static/js/3.31192fe6.chunk.js"
+  },
+  {
+    "revision": "761ed39bfaa91322b3a4",
+    "url": "/react-spa/static/js/4.26c50a31.chunk.js"
+  },
+  {
+    "revision": "5453380a66379f18f028",
+    "url": "/react-spa/static/js/main.08086935.chunk.js"
+  },
+  {
+    "revision": "b7bc188b6211facc8135",
+    "url": "/react-spa/static/js/runtime-main.56adb314.js"
+  },
+  {
+    "revision": "f4d7875d836558dc64082fcc6d7d0b6c",
+    "url": "/react-spa/static/media/preloader.f4d7875d.svg"
+  },
+  {
+    "revision": "68f2a6aace3cfd74492767e21772cffe",
+    "url": "/react-spa/static/media/user.68f2a6aa.svg"
+  }
+]);
